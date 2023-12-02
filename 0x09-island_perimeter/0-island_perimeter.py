c81fa4ff92223fc689d0460b72faffbe802c8321
DevLarry <olanrewajusamad070@gmail.com>
2023-12-02 17:35:48 +0100

    Perimeter of an island

--- 0x09-island_perimeter/0-island_perimeter.py
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -1,42 +1,25 @@
 #!/usr/bin/python3
-"""Perimeter of an island"""
-
-
-def findsingleperimeter(grid, pos):
-    """Get the relative perimeter of one square"""
-    per = 0
-    if pos[0] == 0 or pos[0] == len(grid) - 1:
-        return 0
-    if pos[1] == 0 or pos[1] == len(grid[0]) - 1:
-        return 0
-    if grid[pos[0]-1][pos[1]] == 0:
-        per += 1
-    if grid[pos[0]+1][pos[1]] == 0:
-        per += 1
-    if grid[pos[0]][pos[1]+1] == 0:
-        per += 1
-    if grid[pos[0]][pos[1]-1] == 0:
-        per += 1
-
-    return per
+"""Island perimeter computing module.
+"""
 
 
 def island_perimeter(grid):
-    """inds the perimeter of the island in the grid"""
+    """Computes the perimeter of an island with no lakes.
+    """
     perimeter = 0
-    for i in range(len(grid)):
-        for j in range(len(grid[i])):
-            if grid[i][j] == 1:
-                perimeter += findsingleperimeter(grid, (i, j))
+    if type(grid) != list:
+        return 0
+    n = len(grid)
+    for i, row in enumerate(grid):
+        m = len(row)
+        for j, cell in enumerate(row):
+            if cell == 0:
+                continue
+            edges = (
+                i == 0 or (len(grid[i - 1]) > j and grid[i - 1][j] == 0),
+                j == m - 1 or (m > j + 1 and row[j + 1] == 0),
+                i == n - 1 or (len(grid[i + 1]) > j and grid[i + 1][j] == 0),
+                j == 0 or row[j - 1] == 0,
+            )
+            perimeter += sum(edges)
     return perimeter
-
-
-if __name__ == "__main__":
-    grid = [
-        [0, 0, 0, 0, 0, 0],
-        [0, 1, 0, 0, 0, 0],
-        [0, 1, 0, 0, 0, 0],
-        [0, 1, 1, 1, 0, 0],
-        [0, 0, 0, 0, 0, 0]
-    ]
-    print(island_perimeter(grid))
